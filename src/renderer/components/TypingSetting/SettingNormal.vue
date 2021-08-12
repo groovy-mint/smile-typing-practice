30dfd386dd03d9a7173cb947e402c7ab68728c13
groovy-mint <myebyeol_note@naver.com>
2021-08-12 22:55:33 +0900

    Edited setting design

--- src/renderer/components/TypingSetting/SettingNormal.vue
+++ src/renderer/components/TypingSetting/SettingNormal.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="typeWrapper">
     <p class="mainMenuTitle"><router-link ondragstart="return false" to="*"><LeftIcon/></router-link>환경설정</p><p class="subMenuTitle">일반</p>
-    <h3>언어</h3>
-    <a>한국어</a>
-    <a>영어</a>
-    <a>일어</a>
-    <h3>자판 배열</h3>
-    <a>두벌식</a>
-    <a>세벌식 390</a>
-    <a>세벌식 완성</a>
-    <h3>색각 이상자 옵션</h3>
-    <a>켜기</a>
-    <a>끄기</a>
-    <h3>단어 연습 글자 정렬</h3>
-    <a>가운데</a>
-    <a>왼쪽</a>
+    <div class="settingCon">
+      <b>언어</b> <span>언어를 선택합니다.</span><br>
+      <a>한국어</a>
+      <a>English</a>
+      <a>日本語</a>
+    </div>
+    <div class="settingCon">
+      <b>자판 배열</b>　<span>손에 익힐 자판 배열을 선택합니다.</span><br>
+      <a>두벌식</a>
+      <a>세벌식 390</a>
+      <a>세벌식 완성</a>
+    </div>
+    <div class="settingCon">
+      <b>CUD (색각 이상자용 옵션)</b> <span>오타에 밑줄을 표시하고, 자리 연습의 키보드 색상을 흑백으로 변경합니다.</span><br>
+      <a>켜기</a>
+      <a>끄기</a>
+    </div>
+    <div class="settingCon">
+      <b>단어 연습 글자 정렬</b> <span>단어 연습의 제시어를 왼쪽으로 정렬할지 가운데로 정렬할지 결정합니다.</span><br>
+      <a>가운데</a>
+      <a>왼쪽</a>
+    </div>
   </div>
 </template>
 <script>
@@ -57,5 +65,11 @@ export default {
 }
 .mainMenuTitle a:hover svg{
     background:lightgray
+}
+.settingCon{
+  margin: 30px 0;
+}
+b{
+  font-size: 1.2em;
 }
 </style>
